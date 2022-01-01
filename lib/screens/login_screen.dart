c07b445f30c23b3d215724d6f09176173d27696b
Prathmesh <prathubonde31@gmail.com>
2022-01-02 01:13:49 +0530

    Added Firebase authentication

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -5,6 +5,7 @@ import 'package:Chit_chatter/components/rounded_button.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:Chit_chatter/screens/chat_screen.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -22,6 +23,9 @@ class _LoginScreenState extends State<LoginScreen> {
   String password = "";
   String errorMessage = "";
 
+  bool showSpinner = false;
+  bool _isObscure = true;
+
   void loginErrors(FirebaseAuthException e) {
     if (e.code == 'user-not-found') {
       errorMessage = 'Invalid user or email!';
@@ -36,79 +40,96 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'Logo',
-              child: SizedBox(
-                height: 240.0,
-                child: Image.asset('images/logo1.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'Logo',
+                child: SizedBox(
+                  height: 240.0,
+                  child: Image.asset('images/logo1.png'),
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (value) {
-                email = value;
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                labelText: 'E-Mail',
-                hintText: 'Enter your E-mail',
+              const SizedBox(
+                height: 48.0,
               ),
-              style: const TextStyle(color: Colors.black),
-            ),
-            const SizedBox(
-              height: 16.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              obscureText: true,
-              onChanged: (value) {
-                password = value;
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                labelText: 'Password',
-                hintText: 'Enter your password',
+              TextField(
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  labelText: 'E-Mail',
+                  hintText: 'Enter your E-mail',
+                ),
+                style: const TextStyle(color: Colors.black),
               ),
-              style: const TextStyle(color: Colors.black),
-            ),
-            const SizedBox(
-              height: 28.0,
-            ),
-            RoundedButton(
-              colour: Colors.lightBlueAccent,
-              buttonText: 'Log In',
-              onPressed: () async {
-                try {
-                  final user = await _auth.signInWithEmailAndPassword(email: email, password: password);
-                  if (user != null) {
-                    Navigator.pushNamed(context, ChatScreen.id);
+              const SizedBox(
+                height: 16.0,
+              ),
+              TextField(
+                textAlign: TextAlign.center,
+                obscureText: _isObscure,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  labelText: 'Password',
+                  hintText: 'Enter your password',
+                  suffixIcon: IconButton(
+                    icon: Icon(_isObscure ? Icons.visibility : Icons.visibility_off),
+                    onPressed: () {
+                      setState(() {
+                        _isObscure = !_isObscure;
+                      });
+                    },
+                  ),
+                ),
+                style: const TextStyle(color: Colors.black),
+              ),
+              const SizedBox(
+                height: 28.0,
+              ),
+              RoundedButton(
+                colour: Colors.lightBlueAccent,
+                buttonText: 'Log In',
+                onPressed: () async {
+                  setState(() {
+                    showSpinner = true;
+                  });
+                  try {
+                    final user = await _auth.signInWithEmailAndPassword(email: email, password: password);
+                    if (user != null) {
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    }
+                    setState(() {
+                      showSpinner = false;
+                    });
+                  } on FirebaseAuthException catch (e) {
+                    loginErrors(e);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                          errorMessage,
+                          style: const TextStyle(fontSize: 18.0),
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        backgroundColor: Colors.red.shade400,
+                      ),
+                    );
                   }
-                } on FirebaseAuthException catch (e) {
-                  loginErrors(e);
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text(
-                        errorMessage,
-                        style: const TextStyle(fontSize: 18.0),
-                      ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15.0),
-                      ),
-                      backgroundColor: Colors.red.shade400,
-                    ),
-                  );
-                }
-              },
-            ),
-          ],
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -6,6 +6,7 @@ import 'package:Chit_chatter/screens/chat_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class RegistrationScreen extends StatefulWidget {
   const RegistrationScreen({Key? key}) : super(key: key);
@@ -23,6 +24,9 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   String password = "";
   String cpassword = "";
   String errorMessage = "";
+
+  bool showSpinner = false;
+  bool _isObscure = true;
 
   @override
   void initState() {
@@ -44,109 +48,133 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'Logo',
-              child: SizedBox(
-                height: 200.0,
-                child: Image.asset('images/logo1.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'Logo',
+                child: SizedBox(
+                  height: 200.0,
+                  child: Image.asset('images/logo1.png'),
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 48.0,
-            ),
-            TextFormField(
-              textAlign: TextAlign.center,
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (value) {
-                //Do something with the user input.
-                email = value;
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                labelText: 'E-mail',
-                hintText: 'Enter your E-mail',
+              const SizedBox(
+                height: 48.0,
               ),
-              style: const TextStyle(color: Colors.black),
-            ),
-            const SizedBox(
-              height: 16.0,
-            ),
-            TextFormField(
-              textAlign: TextAlign.center,
-              obscureText: true,
-              onChanged: (value) {
-                //Do something with the user input.
-                password = value;
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                labelText: 'Password',
-                hintText: 'Enter your password',
+              TextFormField(
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) {
+                  //Do something with the user input.
+                  email = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  labelText: 'E-mail',
+                  hintText: 'Enter your E-mail',
+                ),
+                style: const TextStyle(color: Colors.black),
               ),
-              style: const TextStyle(color: Colors.black),
-            ),
-            const SizedBox(
-              height: 16.0,
-            ),
-            TextFormField(
-              textAlign: TextAlign.center,
-              obscureText: true,
-              onChanged: (value) {
-                //Do something with the user input.
-                cpassword = value;
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                labelText: 'Confirm Password',
-                hintText: 'Re-enter password',
+              const SizedBox(
+                height: 16.0,
               ),
-              style: const TextStyle(color: Colors.black),
-            ),
-            const SizedBox(
-              height: 28.0,
-            ),
-            RoundedButton(
-              colour: Colors.blueAccent,
-              buttonText: 'Register',
-              onPressed: () async {
-                try {
-                  if (password == cpassword) {
-                    final newUser = await _auth.createUserWithEmailAndPassword(email: email, password: password);
-                    if (newUser != null) {
-                      Navigator.pushNamed(context, ChatScreen.id);
-                    }
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text(
-                          "Password don't match!",
-                          style: TextStyle(fontSize: 18.0),
+              TextFormField(
+                textAlign: TextAlign.center,
+                obscureText: _isObscure,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  labelText: 'Password',
+                  hintText: 'Enter your password',
+                  suffixIcon: IconButton(
+                    icon: Icon(_isObscure ? Icons.visibility : Icons.visibility_off),
+                    onPressed: () {
+                      setState(() {
+                        _isObscure = !_isObscure;
+                      });
+                    },
+                  ),
+                ),
+                style: const TextStyle(color: Colors.black),
+              ),
+              const SizedBox(
+                height: 16.0,
+              ),
+              TextFormField(
+                textAlign: TextAlign.center,
+                obscureText: _isObscure,
+                onChanged: (value) {
+                  //Do something with the user input.
+                  cpassword = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  labelText: 'Confirm Password',
+                  hintText: 'Re-enter password',
+                  suffixIcon: IconButton(
+                    icon: Icon(_isObscure ? Icons.visibility : Icons.visibility_off),
+                    onPressed: () {
+                      setState(() {
+                        _isObscure = !_isObscure;
+                      });
+                    },
+                  ),
+                ),
+                style: const TextStyle(color: Colors.black),
+              ),
+              const SizedBox(
+                height: 28.0,
+              ),
+              RoundedButton(
+                colour: Colors.blueAccent,
+                buttonText: 'Register',
+                onPressed: () async {
+                  setState(() {
+                    showSpinner = true;
+                  });
+                  try {
+                    if (password == cpassword) {
+                      final newUser = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+                      if (newUser != null) {
+                        Navigator.pushNamed(context, ChatScreen.id);
+                      }
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text(
+                            "Password don't match!",
+                            style: TextStyle(fontSize: 18.0),
+                          ),
+                          backgroundColor: Color(0xFFBBDEFB),
                         ),
-                        backgroundColor: Color(0xFFBBDEFB),
+                      );
+                    }
+                    setState(() {
+                      showSpinner = false;
+                    });
+                  } on FirebaseAuthException catch (e) {
+                    signupErrors(e);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                          errorMessage,
+                          style: const TextStyle(fontSize: 18.0),
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        backgroundColor: const Color(0xFFBBDEFB),
                       ),
                     );
                   }
-                } on FirebaseAuthException catch (e) {
-                  signupErrors(e);
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text(
-                        errorMessage,
-                        style: const TextStyle(fontSize: 18.0),
-                      ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15.0),
-                      ),
-                      backgroundColor: const Color(0xFFBBDEFB),
-                    ),
-                  );
-                }
-              },
-            ),
-          ],
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
